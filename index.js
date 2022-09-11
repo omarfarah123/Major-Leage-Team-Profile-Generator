8a3ba892604a5df613fd28e921b56b95187ec807
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-09-10 22:47:32 -0400

    Adjusted some issues regarding validation

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
+//Inquirer library used for taking input
 const inquirer = require('inquirer');
+//FS library for creating files
 const fs = require('fs');
+//Chalk library used for command line colors
 const chalk = require('chalk');
+//All of the classes being imported
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
 
-const inputValidator = async (input) => {
-    if (input === '') {
-       return 'Please enter valid input';
-    }
-    return true;
- };
 
 
 
-
+//Array containing members being added to the team
 const employeeTeam = [];
+//Array containing the ids of the employees added
 const employeeIDs = [];
     console.log("Major Leage Team Generator")
+    //Taking in data for the manager
     inquirer
     .prompt([
         {
             type: "input",
             name: "name",
             message: "Please enter the name of the manager?",
+            //Checks if name being entered is valid
             validate: function(name){
-                if(name === ""){
+                if(name.trim().length === 0){
                     console.log(chalk.red("Please enter a name."))
                     return false;
                 } else {
@@ -39,8 +40,9 @@ const employeeIDs = [];
             type: "input",
             name: "employeeId",
             message: "Please enter the employee Id",
+            //Checks if employee id being entered is valid 
             validate : function (employeeId){
-                if(employeeId === ""){
+                if(employeeId.trim().length === 0){
                   console.log(chalk.red("No ID entered"))
                   return false;
                 } else if (employeeIDs.includes(employeeId)) {
@@ -55,8 +57,10 @@ const employeeIDs = [];
             type: "input",
             name: "email",
             message: "Please enter the manager's email address?",
+            //Chacks if email being entered is valid
             validate: function (email) {
-  
+                
+                //Checks if data entered passed this regex test
                 valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
     
                 if (valid) {
@@ -72,17 +76,26 @@ const employeeIDs = [];
             type: "input",
             name: "officeNumber",
             message: "Please enter the Manager's office number?",
-            
-            //validate: inputValidator()
+            validate : function(officeNumber){
+                if(officeNumber.trim().length === 0){
+                    console.log(chalk.red("Please enter an office number"))
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         }
     ]).then((response) => {
+        //Created a new Manager object with data entered
         employeeTeam.push(new Manager(response.name, response.employeeId, response.email, response.officeNumber))
         employeeIDs.push(response.employeeId)
         console.log(chalk.green(`Manager ${response.name} added to team`))
         main()
     })
 
+
 function main(){
+    //Main function to allow user to choose what kind of person to add to the team
     inquirer
     .prompt([
         {
@@ -112,7 +125,7 @@ function engineer(){
             name: "name",
             message: "Please enter the name of the Engineer?",
             validate: function(name){
-                if(name === ""){
+                if(name.trim().length === 0){
                     console.log(chalk.red("Please enter a name."))
                     return false;
                 } else {
@@ -125,7 +138,7 @@ function engineer(){
             name: "employeeId",
             message: "Please enter the Employee Id",
             validate : function (employeeId){
-                if(employeeId === ""){
+                if(employeeId.trim().length === 0){
                   console.log(chalk.red("No ID entered"))
                   return false;
                 } else if (employeeIDs.includes(employeeId)) {
@@ -158,7 +171,7 @@ function engineer(){
             name: "github",
             message: "Please enter the Engineer's github username?",
             validate : function (github){
-                if(github === ""){
+                if(github.trim().length === 0){
                     console.log(chalk.red("Please enter a github username"))
                 } else {
                     return true;
@@ -166,6 +179,7 @@ function engineer(){
             }
         }
     ]).then((response) => {
+        //Adds an Engineer to the team by creating an object with the values entered
         employeeTeam.push(new Engineer(response.name, response.employeeId, response.email, response.github))
         console.log(chalk.green(`Engineer ${response.name} added to team`))
         main();
@@ -180,7 +194,7 @@ function intern(){
             name: "name",
             message: "Please enter the name of the Intern?",
             validate: function(name){
-                if(name === ""){
+                if(name.trim().length === 0){
                     console.log(chalk.red("Please enter a name."))
                     return false;
                 } else {
@@ -193,7 +207,7 @@ function intern(){
             name: "employeeId",
             message: "Please enter the Intern Id",
             validate : function (employeeId){
-                  if(employeeId === ""){
+                  if(employeeId.trim().length === 0){
                     console.log(chalk.red("No ID entered"))
                     return false;
                   } else if (employeeIDs.includes(employeeId)) {
@@ -212,20 +226,24 @@ function intern(){
   
                 valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
     
-                if (valid) {
-                    return true;
-                } else {
+                if (email.trim().length === 0) {
+                    console.log(chalk.red("No email entered!"))
+                    return false;
+                } else if(!valid) {
                     console.log(chalk.red(" Please enter a valid email"))
                     return false;
+                } else {
+                    return true;
                 }
            }   
         },
         {
             type: "input",
-            name: "github",
+            name: "school",
             message: "Please enter the Intern's school?",
             validate: function(school){
-                if(school === ""){
+                if(school.trim().length === 0){
+                    console.log(chalk.red("No school entered"))
                     return false;
                 } else {
                     return true;
@@ -239,17 +257,20 @@ function intern(){
         main()
     })
 }
-
+//This function removes members which are selected for removal
 function removeMember(){
     inquirer.prompt([
         {
             type: "list",
             name: "removal",
             message: "Please select a member to remove",
+            //Displays a choice list of all the members which have been created and have been added
             choices : employeeTeam
         }
     ]).then((response) => {
+        //Finds the index where the employee is located
         const memberIndex = employeeTeam.findIndex((teamMember) => teamMember == response.removal);
+        //Once found remove the member
         employeeTeam.splice(memberIndex, 1)
         console.log(chalk.green(`Intern ${response.removal} has been deleted!`));
         main()
@@ -257,9 +278,11 @@ function removeMember(){
 }
 
 function pageCreation(){
+    //If there are no members on the team then alert user
     if(employeeTeam.length === 0){
         console.log(chalk.red("No members were found on the roster!!!"))
     } else {
+        //HTML page generated for the team
         let html = `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -268,7 +291,7 @@ function pageCreation(){
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
-        <title>Team</title>
+        <title>Major League Team</title>
     </head>
     <body>
         <div class="container-fluid p-3 bg-primary text-white text-center">
@@ -307,6 +330,7 @@ function pageCreation(){
     `</div>
     </body>
     </html>`;
+    //If the file already exists meaning old generated page then delete
     if (fs.existsSync(`${__dirname}\\dist\\index.html`)) {
         try {
             fs.unlinkSync(`${__dirname}\\dist\\index.html`)
@@ -315,9 +339,11 @@ function pageCreation(){
             console.error(err);
         }
     }
+    //File creation with team added
     fs.appendFile(`${__dirname}\\dist\\index.html`, html, function (err) {
         if (err) throw err;
         console.log('File has been saved!');
+        //Opens the location where the file lives
         require('child_process').exec(`start "" "${__dirname}\\dist"`)
       });
     }
